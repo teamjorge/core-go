32e91067acd395bdde75efcb68fabd701cbeb90f
teamjorge <18563449+teamjorge@users.noreply.github.com>
2020-11-09 08:19:28 +0200

    Feat/add docs (#7)
    * Base commit
    
    * Update README
    
    Add Sets docs
    
    * Add spacing between features
    
    * Add pkg files per package.
    
    Added a word to slice generic docstring
    
    * Newline on Sets
    
    * Add Slices/Chars docs
    Update some doc strings
    Update gitignore
    
    * Rename v1 package init to pkg.go
    
    * Change unit testing command to target v1 package.
    Update README to reflect correct unit test command

---
+++ v1/slices/pkg.go
@@ -0,0 +1,4 @@
+// Package slices provides helper methods for working with slices. Commonly used
+// types in go are supported outright. However, there are generic functions and interfaces
+// to support user defined data types.
+package slices
