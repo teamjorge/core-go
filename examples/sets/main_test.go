32e92850d20db1f669f70ccf2af88fa74018788b
teamjorge <georgevanr@gmail.com>
2020-11-17 08:45:57 +0200

    Add sanity Unit Tests for examples Fixes #18

---
+++ examples/sets/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Make sure it works",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
